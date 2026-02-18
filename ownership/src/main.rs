368c933d1d616947eec9f5283108bc12391ba610
jovani <163670507+lipatovSergey@users.noreply.github.com>
2026-02-18 21:18:31 +0200

    start 4.3 rust book slices lesson

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,7 +1,9 @@
 mod borrowing_and_references;
+mod slices;
 mod stack_heap_allocated;
 
 fn main() {
     stack_heap_allocated::run_examples();
     borrowing_and_references::run_examples();
+    slices::run_examples();
 }
